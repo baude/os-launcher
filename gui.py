aa48d63ca5c4dafcaf58333b8a4c281f1993b5e7
Brent Baude <bbaude@redhat.com>
2017-06-22 13:42:50 -0500

    UPdate prior to class'ing it up!

--- gui.py
+++ gui.py
@@ -1,9 +1,13 @@
 import yaml
-from openstack_helpers import get_image_info, get_flavors
+from openstack_helpers import get_image_info, get_flavors, get_instances, delete_instance, shutdown_instance, start_instance, get_server_info
 import sys
 import subprocess
 from tempfile import NamedTemporaryFile
+from novaclient.exceptions import Conflict
 
+
+progress_bar = None
+tree_columns = ("name", "ip", "id")
 
 def load_defaults():
     with open("launcher.conf", 'r') as stream:
@@ -11,7 +15,29 @@ def load_defaults():
         return conf_yaml['default_flavor'], conf_yaml['default_image'], conf_yaml['default_yaml_file_path'], conf_yaml['default_instance_name']
 
 default_flavor, default_image, default_yaml_file_path, default_instance_name = load_defaults()
+servers = None
+
+os_flavors = None
+
+
+
+def get_instance_flavors(refresh=False):
+    global os_flavors
+    if refresh:
+        return get_flavors(refresh=True)
+    if os_flavors is None:
+        return get_flavors()
+    return os_flavors
+
 default_flavor_id = get_flavors().get_id_from_flavor(default_flavor)
+
+def get_servers():
+    global servers
+    servers = get_instances()
+    return servers.all
+
+#baudes = servers.find_instances()
+#print(len(baudes))
 
 def check_if_python2():
     if int(sys.version_info[0]) < 3:
@@ -20,10 +46,41 @@ def check_if_python2():
         return False
 if check_if_python2():
     from Tkinter import *
+    import tkMessageBox as messagebox
+    import ttk
 else:
     from tkinter import *
+    from tkinter import ttk
+    from tkinter import tkMessageBox as messagebox
+
+
+
 
 def submit(listbox, flavors, mainframe, *args):
+    def add_name_and_counter(name, flavor):
+        new_name = "{}-{}-".format(name, flavor)
+        global servers
+        _servers = servers.find_instances(search_value=new_name)
+        print("$$$$$$$$$$$$$")
+        print(len(_servers))
+        print("$$$$$$$$$$$$$")
+        if len(_servers) < 1:
+            return new_name + "1"
+        else:
+            print("y")
+            print(new_name)
+            used_counters = []
+            count_range = range(1,11)
+            print(count_range)
+            for i in [x.name for x in _servers]:
+                print("-->{}".format(i))
+                used_counters.append(int(i.replace(new_name,"")))
+            print(used_counters)
+            new_counter = min([x for x in range(1,11) if x not in used_counters])
+            return new_name + "{}".format(new_counter)
+
+    global entry_name
+    _name = entry_name.get()
     try:
         index_select = int(listbox.curselection()[0])
         flavor_select = int(flavors.curselection()[0])
@@ -32,9 +89,10 @@ def submit(listbox, flavors, mainframe, *args):
         return
 
     image = get_image_info().list_of_names()[index_select]
-    name = get_flavors().list_of_names()[flavor_select]
-    id = get_flavors().get_id_from_flavor(name)
-    launch_instance(image, name, id)
+    flavor_name = get_instance_flavors().list_of_names()[flavor_select]
+    id = get_instance_flavors().get_id_from_flavor(flavor_name)
+    instance_name = add_name_and_counter(_name, flavor_name)
+    launch_instance(image, instance_name, id)
 
 def refresh(listbox):
     listbox.delete(0, END)
@@ -45,7 +103,7 @@ def refresh(listbox):
 
 def refresh_flavor(listbox):
     listbox.delete(0, END)
-    flavors = get_flavors(refresh=True)
+    flavors = get_instance_flavors(refresh=True)
     for item in flavors.list_of_names():
         listbox.insert(END, item)
 
@@ -71,13 +129,20 @@ def form_yaml(image, name, id):
     f.close()
     return f.name
 
-
+def foo():
+    global tree
+    bar = tree.focus()
+    id = tree.item(bar)['values'][2]
+    return servers.get_instance_by_id(id)
 
 def launch_instance(image, name, id):
     # ansible-playbook  osp7-testing.yml -e 'name=baude image=Fedora-Cloud-Base-26-20170605.n.0'
-    yaml_file_name = form_yaml(image, default_instance_name, id)
+    yaml_file_name = form_yaml(image, name, id)
     cmd = ['ansible-playbook', '-vvv', yaml_file_name]
     print(" ".join(cmd))
+    subp(cmd)
+
+def subp(cmd):
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     while True:
         output = process.stdout.readline()
@@ -86,22 +151,137 @@ def launch_instance(image, name, id):
         if output:
             print output.strip()
         rc = process.poll()
-    _exit()
+    return rc
 
 def populate_listbox(listbox, values):
     for item in values:
         listbox.insert(END, item)
 
+def context_menu(event):
+    global popup
+    try:
+        #popup.tk_popup(event.x_root, event.y_root, 0)
+        popup.post(event.x_root, event.y_root)
+    except Exception as e:
+        print(str(e))
+        sys.exit()
+
+def delete_instances(tree):
+    for i in tree.get_children():
+        tree.delete(i)
+
+def refresh_instances(tree):
+    global progress_bar
+    global servers
+    progress_bar.start()
+    delete_instances(tree)
+    populate_servers(get_servers())
+    progress_bar.stop()
+
+def delete_server():
+    server = foo()
+    delete_instance(server.id)
+
+def stop_server():
+    server = foo()
+    try:
+        shutdown_instance(server.id)
+    except Conflict as e:
+        return messagebox.showerror("Error", str(e))
+
+def start_server():
+    server = foo()
+    try:
+        start_instance(server.id)
+    except Conflict as e:
+        return messagebox.showerror("Error", str(e))
+
+def copy_to_clipboard():
+    global root
+    server = foo()
+    root.clipboard_clear()
+    root.clipboard_append(server.ip)
+
+def run_playbook():
+    global root
+    server = foo()
+    cmd = ['ansible-playbook', '-vv', '-i', '{},'.format(server.ip), '-u', 'cloud-user', 'playbook.yml']
+    print(cmd)
+    subp(cmd)
+
+def populate_servers(servers):
+    global tree
+    print("Server Type is : {}".format(servers))
+    for server in servers:
+        id = tree.insert('', 'end', values=(server.name, server.ip, server.id))
+        tree.insert(id, 'end', values=("", "State", server.state))
+        tree.insert(id, 'end', values=("", "Flavor ID", server.flavor))
+        tree.insert(id, 'end', values=("", "Flavor Name", get_instance_flavors().get_flavor_name_from_id(server.flavor)))
+
+
+def server_filter(filter_var, tree):
+    global servers
+    _filter = filter_var.get()
+    delete_instances(tree)
+    if servers is None:
+        servers = get_instances()
+    if _filter is not None:
+        print("here1")
+        filtered_servers = servers.find_instances(search_value=_filter)
+        print("here2")
+    else:
+        filtered_servers = servers
+    populate_servers(filtered_servers)
+
+def image_filter():
+    def filter_for_value(_image_name_list, value):
+        _filtered = []
+        for i in _image_name_list:
+            if i.lower().find(value.lower()) > -1:
+                _filtered.append(i)
+        return _filtered
+
+    global _fedora
+    global _rhel
+    global _windows
+    global images
+    global listbox
+    fedora = _fedora.get()
+    rhel = _rhel.get()
+    windows = _windows.get()
+    filtered_images = []
+    # populate_listbox(listbox, images.list_of_names())
+    image_name_list = images.list_of_names()
+    listbox.delete(0, END)
+    if not fedora and not rhel and not windows:
+        populate_listbox(listbox, image_name_list)
+    if fedora:
+        filtered_images += filter_for_value(image_name_list, "fedora")
+    if rhel:
+        filtered_images += filter_for_value(image_name_list, "rhel")
+    if windows:
+        filtered_images += filter_for_value(image_name_list, "windows")
+
+    populate_listbox(listbox, filtered_images)
+
+
+
+
 images = get_image_info()
 root = Tk()
 root.title("Tk dropdown example")
+
+_fedora = IntVar()
+_rhel = IntVar()
+_windows = IntVar()
 
 # Add a grid
 mainframe = Frame(root)
 mainframe.grid(column=0,row=0) #sticky=(N,W,E,S) )
 mainframe.columnconfigure(0, weight = 1)
 mainframe.rowconfigure(0, weight = 1)
-mainframe.pack(pady = 300, padx = 200)
+#mainframe.pack(pady = 300, padx = 200)
+bottom_frame = Frame(root)
 
 # Create a Tkinter variable
 tkvar = StringVar(root)
@@ -114,27 +294,69 @@ scrollbar = Scrollbar(mainframe, orient=VERTICAL)
 fscrollbar = Scrollbar(mainframe, orient=VERTICAL)
 listbox = Listbox(mainframe, width=max_image_length, yscrollcommand=scrollbar.set, exportselection=0)
 flavors = Listbox(mainframe, yscrollcommand=fscrollbar.set, exportselection=0)
+entry_name = Entry(mainframe, textvariable="foobar")
+entry_name.insert(0, default_instance_name)
 
-listbox.grid(row=1, column=6, columnspan=2, rowspan=4)
-flavors.grid(row=1, column=2, columnspan=2, rowspan=4)
-scrollbar.grid(row=1, column=8, rowspan=4, sticky=N+S)
-fscrollbar.grid(row=1, column=4, rowspan=4, sticky=N+S)
+listbox.grid(row=3, column=6, columnspan=2, rowspan=4)
+flavors.grid(row=3, column=2, columnspan=2, rowspan=4)
+scrollbar.grid(row=3, column=8, rowspan=4, sticky=N+S)
+fscrollbar.grid(row=3, column=4, rowspan=4, sticky=N+S)
+entry_name.grid(row=0, column=2, columnspan=2)
+
 scrollbar['command'] = listbox.yview
 fscrollbar['command'] = flavors.yview
 populate_listbox(listbox, images.list_of_names())
-populate_listbox(flavors, get_flavors().list_of_names())
-Label(mainframe, text="Images").grid(row = 0, column = 6, columnspan=2)
-Label(mainframe, text="Flavors").grid(row = 0, column = 2, columnspan=2)
+populate_listbox(flavors, get_instance_flavors().list_of_names())
+Label(mainframe, text="Images").grid(row = 2, column = 5)
+fedora = Checkbutton(mainframe, text="fedora", variable=_fedora, command=image_filter).grid(row=2, column=6)
+rhel = Checkbutton(mainframe, text="rhel", variable=_rhel, command=image_filter).grid(row=2, column=7)
+windows = Checkbutton(mainframe, text="windows", variable=_windows, command=image_filter).grid(row=2, column=8)
 
-Label(mainframe, text = '').grid(row=5, column=0)
-Button(mainframe, text="OK", command=lambda: submit(listbox, flavors, mainframe)).grid(row=7, column=6, rowspan=2)
-Button(mainframe, text="Exit", command=_exit).grid(row=7, column=7, rowspan=2)
-Button(mainframe, text="Refresh", command=lambda: refresh(listbox)).grid(row=2, column=5, rowspan=3)
-Button(mainframe, text="Refresh", command=lambda: refresh_flavor(flavors)).grid(row=2, column=0, rowspan=3)
+Label(mainframe, text="Flavors").grid(row = 2, column = 2, columnspan=2)
+Label(mainframe, text="Instance Name").grid(row = 0, column = 0, columnspan=2)
+Label(mainframe, text = '').grid(row=1, column=0)
+Label(mainframe, text = '').grid(row=7, column=0)
+Button(mainframe, text="Launch", command=lambda: submit(listbox, flavors, mainframe)).grid(row=9, column=6, rowspan=2)
+Button(mainframe, text="Exit", command=_exit).grid(row=9, column=7, rowspan=2)
+Label(mainframe, text = '').grid(row=11, column=0)
+Button(mainframe, text="Refresh", command=lambda: refresh(listbox)).grid(row=4, column=5, rowspan=3)
+Button(mainframe, text="Refresh", command=lambda: refresh_flavor(flavors)).grid(row=4, column=0, rowspan=3)
 
 
 # Set Defaults
 listbox.select_set(get_image_info().list_of_names().index(default_image))
-flavors.select_set(get_flavors().list_of_names().index(default_flavor))
+flavors.select_set(get_instance_flavors().list_of_names().index(default_flavor))
+
+
+Label(mainframe, text="Servers").grid(row = 10, column = 0, columnspan=2)
+fv = StringVar()
+tree = ttk.Treeview(columns=tree_columns, show="headings")
+fv.trace("w", lambda name, index, mode, sv=fv, tree=tree: server_filter(sv, tree))
+
+filter_name= Entry(mainframe, textvariable=fv)
+filter_name.grid(row=10, column=2, columnspan=2)
+vsb = Scrollbar(orient="vertical", command=tree.yview)
+hsb = Scrollbar(orient="horizontal", command=tree.xview)
+tree.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
+tree.grid(column=2, row=13, columnspan=6, sticky='nsew', in_=mainframe)
+mainframe.grid_columnconfigure(0, weight=1)
+mainframe.grid_rowconfigure(0, weight=1)
+Button(mainframe, text="Refresh", command=lambda: refresh_instances(tree)).grid(row=12, column=0, rowspan=3)
+
+for col in tree_columns:
+    tree.heading(col, text=col.title())
+
+popup = Menu(root, tearoff=0)
+popup.add_command(label="Delete", command=delete_server) # , command=next) etc...
+popup.add_command(label="Stop", command=stop_server)
+popup.add_command(label="Start", command=start_server)
+popup.add_command(label="Copy to clipboard", command=copy_to_clipboard)
+popup.add_command(label="Run playbook", command=run_playbook)
+populate_servers(get_servers())
+tree.bind("<Button-3>", context_menu)
+Label(mainframe, text="").grid(row=14, column=0, columnspan=5)
+message = Label(mainframe, text="").grid(row=18, column=0, columnspan=5)
+progress_bar = ttk.Progressbar(mainframe, orient=HORIZONTAL, length=200, mode='indeterminate').grid(row=18, columnspan=7)
+Label(mainframe, text="").grid(row=19, column=0, columnspan=5)
 
 root.mainloop()
--- openstack_helpers.py
+++ openstack_helpers.py
@@ -2,37 +2,57 @@ from shade import *
 import json
 import os
 from munch import Munch
+from novaclient import client
+from keystoneauth1 import loading
+from keystoneauth1 import session
+import glanceclient.v2.client as glclient
+
 
 IMAGE_FILE = os.path.join(os.path.expanduser('~'), ".openstack_images.json")
 FLAVORS = os.path.join(os.path.expanduser('~'), ".openstack_flavors.json")
 
 simple_logging(debug=True)
 conn = openstack_cloud(cloud='e2e')
+loader = loading.get_plugin_loader('password')
+auth = loader.load_from_options(auth_url=os.environ['OS_AUTH_URL'],
+                                username=os.environ['OS_USERNAME'],
+                                password=os.environ['OS_PASSWORD'],
+                                project_id=os.environ['OS_TENANT_ID'])
+sess = session.Session(auth=auth)
+nova = client.Client(2, session=sess)
+glance = glclient.Client(session=sess)
+
 
 class OS_Distros(object):
     def __init__(self, image):
         self.name = None
         self.status = None
-        self.os_distro = None
-        self.major_version = None
-        self.orig = None
+        self.os_type = None
+        #self.os_distro = None
+        #self.major_version = None
         self.input = image
+        '''
+                    meta_os_type = None if not hasattr(i, "meta_os_type") else i.meta_os_type
+            if meta_os_type is None and hasattr(i, "os_type"):
+                meta_os_type = i.os_type
+        '''
 
     def create_from_openstack(self):
         self.name = self.input.get("name", "") or ""
         self.status = self.input.get("status", "") or ""
-        self.major_version = "" if not hasattr(self.input, "os_major_ver") else getattr(self.input, "os_major_ver")
-        self.os_type = "" if not hasattr(self.input, "os_type") else getattr(self.input, "os_type")
-        self.os_variant = "" if not hasattr(self.input, "os_variant") else getattr(self.input, "os_variant")
-        self.os_distro = "" if not hasattr(self.input, "os_distro") else getattr(self.input, "os_distro")
+        self.os_type = None if not hasattr(self.input, "meta_os_type") else self.input.meta_os_type
+        if self.os_type is None and hasattr(self.input, "os_type"):
+            self.os_type = self.input.os_type
+        elif self.os_type is None and hasattr(self.input, "metadata"):
+            if hasattr(self.input.metadata, 'meta_os_type'):
+                self.os_type = self.input.metadata.meta_os_type
+            else:
+                self.os_type = ""
 
-    def create_from_cache(self):
-        self.status = self.input["status"]
-        self.name = self.input["name"]
-        self.os_distro = self.input["os_distro"]
-        self.major_version = getattr(self.input, "major_version", '')
-        self.os_variant = self.input["os_variant"]
-        self.os_type = self.input["os_type"]
+    #def create_from_cache(self):
+    #    self.status = self.input["status"]
+    #    self.name = self.input["name"]
+    #    self.os_type = self.input["os_type"]
 
     def get_value(self, value):
         return self.__getattribute__(value)
@@ -40,10 +60,7 @@ class OS_Distros(object):
     def to_dict(self):
         return { "name": self.name.encode('utf-8'),
                  "status": self.status.encode('utf-8'),
-                 "major_version:": self.major_version.encode('utf-8'),
                  "os_type": self.os_type.encode('utf-8'),
-                 "os_variant": self.os_variant.encode('utf-8'),
-                 "os_distro": self.os_distro.encode('utf-8')
                  }
 
 class Images(object):
@@ -54,11 +71,7 @@ class Images(object):
     def create(self):
         for i in self.input_images:
             _tmp = OS_Distros(i)
-            if isinstance(i, Munch):
-                _tmp.create_from_openstack()
-            else:
-                _tmp.create_from_cache()
-
+            _tmp.create_from_openstack()
             self.image_list.append(_tmp)
 
     def dump(self, value_to_dump=None):
@@ -69,13 +82,13 @@ class Images(object):
                 print(i.get_value(value_to_dump))
 
     def sort_by(self, value):
-        newlist = sorted([x for x in self.image_list if x.os_type == "linux"], key=lambda k: k.name)
+        newlist = sorted([x for x in self.image_list if x.name is not ""], key=lambda k: k.name)
         return newlist
 
     def to_json(self):
         my_list = []
         for i in self.image_list:
-            if i.os_type == 'linux':
+            if i.name is not "":
                 my_list.append(i.to_dict())
         return my_list
 
@@ -178,6 +191,80 @@ class Flavors(object):
             if x.name == name:
                 return x.get_value('id')
 
+    def get_flavor_name_from_id(self, id):
+        for x in self.sort_by("foo"):
+            if x.id == int(id):
+                return x.get_value('name')
+
+class Instance(object):
+    def __init__(self, input):
+        self.input = input
+        self.name = None
+        self.id = None
+        self.state = None
+        self.user_id = None
+        self.ip = None
+        self.flavor = None
+
+    def create(self):
+        self.name = self.input['name']
+        self.id = self.input['id']
+        self.state = self.input['vm_state']
+        self.user_id = self.input['user_id']
+        self.ip = self.input['accessIPv4']
+        self.flavor = self.input['flavor']['id']
+
+
+    def get_value(self, value):
+        return self.__getattribute__(value)
+
+    def dump(self):
+        print("name={}, id={}, state={}, user_id={}, ip={}, input={}".format(self.name,
+                                                                   self.id,
+                                                                   self.state,
+                                                                   self.user_id,
+                                                                   self.ip,
+                                                                             self.input))
+
+class Instances(object):
+    def __init__(self, input):
+        self.input = input
+        self.instance_list = []
+
+    def create(self):
+        for i in self.input:
+            _tmp = Instance(i)
+            _tmp.create()
+            self.instance_list.append(_tmp)
+
+    def dump(self, value_to_dump=None):
+        for i in self.instance_list:
+            if not value_to_dump:
+                print(vars(i))
+            else:
+                print(i.get_value(value_to_dump))
+
+    def find_instances(self, search_value=None):
+        if not search_value:
+            return self.instance_list
+
+        search_list = []
+        for i in self.instance_list:
+            print("searching for {} in {}".format(search_value, i.name))
+            if i.name.find(search_value) > -1:
+                search_list.append(i)
+        print("search list is {}".format(search_list))
+        return search_list
+
+    def get_instance_by_id(self, id):
+        for i in self.instance_list:
+            if i.id == id:
+                return i
+        return None
+
+    @property
+    def all(self):
+        return [x for x in self.instance_list]
 
 def load_images_from_cache():
     return json.loads(open(IMAGE_FILE, "r").read())
@@ -193,9 +280,8 @@ def get_image_info(refresh=False):
         os_images = load_images_from_cache()
     images = Images(os_images)
     images.create()
-    if not os.path.exists(IMAGE_FILE):
+    if refresh or not os.path.exists(IMAGE_FILE):
         images.to_file(images.to_json())
-
     return images
 
 
@@ -209,3 +295,39 @@ def get_flavors(refresh=False):
     if not os.path.exists(FLAVORS):
         flavors.to_file(flavors.to_json())
     return flavors
+
+def get_instances():
+    servers_list = conn.list_servers()
+    servers = Instances(servers_list)
+    servers.create()
+    return servers
+
+def delete_instance(server_id):
+    conn.delete_server(server_id, wait=False)
+
+def shutdown_instance(server_id):
+    server = nova.servers.find(id=server_id)
+    server.stop()
+
+def start_instance(server_id):
+    server = nova.servers.find(id=server_id)
+    server.start()
+
+def get_server_info(server_id):
+    server = nova.servers.find(id=server_id)
+    print(server)
+    print(vars(server))
+    print("@@@@@@@@@@@@@@@@2")
+    print("keys: {}".format([x for x in vars(server)]))
+    #print("image flavor: {}".format(server['flavor']['id']))
+    #print(server['OS-EXT-STS:task_state'])
+    print("**************")
+    print(server.flavor['id'])
+    print(server.name)
+    print(server.id)
+    print(server.addresses)
+    print(server.to_dict())
+    print("**************")
+#def redeploy_instance(server_id):
+#    server = nova.servers.find(id=server_id)
+#    server.()
